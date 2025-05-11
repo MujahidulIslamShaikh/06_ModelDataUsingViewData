669cdabe2781f87e506e8ae13a038f48eab5234b
MujahidulIslamShaikh <codewith247@gmail.com>
2025-05-11 15:12:29 -0700

    Initialized

---
+++ ModelDataUsingViewData/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ModelDataUsingViewData.Models;
+
+namespace ModelDataUsingViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Employee emp = new Employee() 
+            {
+                EmpId = 1001,
+                EmpName = "Mujahid",
+                Designation = "Manager",
+                Salary = 25000,
+            };
+            ViewData["myEmployee"] = emp;
+            ViewBag.myEmpData = emp;
+            TempData["myEmployee"] = emp;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var myEmployee = new List<Employee>
+            {
+                new Employee { EmpId=101,EmpName="Sahil",Designation="Manager",Salary=25000},
+                new Employee { EmpId=102,EmpName="Mujahid",Designation="Developer",Salary=25000},
+                new Employee { EmpId=103,EmpName="Zubair",Designation="Programmer",Salary=25000}
+            };
+            ViewData["empData"] = myEmployee;
+            //ViewBag.empData = myEmployee;
+            //TempData["empData"] = myEmployee;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
